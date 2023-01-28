6131ee65cc2fbce6d1d4e5182c81e230be8a2a7f
KetikovAlexander <spok800@yandex.ru>
2023-01-28 22:50:06 +0300

    Add final programm C#

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string [] array = {"Hello", "Hi", "323233", "5434343", ":)", "Привет", "The", "End"};
+int b = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+int a = array[i].Length;
+
+if (a <= 3)
+{
+    b++;
+}
+}
+string [] array2 = new string [b]; 
+
+b = 0;
+for(int i = 0; i < array.Length; i++)
+{
+int a = array[i].Length;
+
+if (a <= 3)
+{
+    array2[b] = array[i];
+    b++;
+}
+}
+
+Console.WriteLine($"[{string.Join(", ", array2)}]");
