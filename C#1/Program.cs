d5215df5fd531b6dfcea3964c66c13b66179c010
KetikovAlexander <spok800@yandex.ru>
2023-01-29 19:04:22 +0300

    Add second variant of TASK with minimum code

---
+++ C#1/Program.cs
@@ -0,0 +1,14 @@
+﻿string [] array = {"Hello", "Hi", "323233", "5434343", ":)", "Привет", "The", "End"};
+int b = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+int a = array[i].Length;
+
+if (a <= 3)
+{
+    array[b] = array[i];
+    b++;
+}
+}
+Console.WriteLine($"[{string.Join(" ", array.OrderBy(x => b).Take(b))}]");
